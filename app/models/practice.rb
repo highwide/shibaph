6bd38775969a4ca468bd74f119ecea1af5af087f
Takahiro Uchiyama <hochweit728@gmail.com>
2015-06-21 17:40:16 +0900

    Add validates and relation to models

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,7 @@
 class Practice < ActiveRecord::Base
+  belongs_to :user
+  has_many   :daily_progresses
+  validates  :user_id, presence: true
+  validates  :title,   presence: true, length: { maximum: 50 }
+  validates  :unit,    presence: true, length: { maximum: 10 }
 end
