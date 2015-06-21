49c9c6bc4d823e6530c6f284b9c87ba7a71c867c
ta1kt0me <p.wadachi@gmail.com>
2015-06-21 22:56:06 +0900

    Exec annotate

--- app/models/daily_progress.rb
+++ app/models/daily_progress.rb
@@ -1,3 +1,20 @@
+# ## Schema Information
+#
+# Table name: `daily_progresses`
+#
+# ### Columns
+#
+# Name               | Type               | Attributes
+# ------------------ | ------------------ | ---------------------------
+# **`id`**           | `integer`          | `not null, primary key`
+# **`practice_id`**  | `integer`          |
+# **`done_at`**      | `date`             |
+# **`goal`**         | `integer`          |
+# **`progress`**     | `integer`          |
+# **`created_at`**   | `datetime`         | `not null`
+# **`updated_at`**   | `datetime`         | `not null`
+#
+
 class DailyProgress < ActiveRecord::Base
   belongs_to :practice
   validates  :practice_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# ## Schema Information
+#
+# Table name: `users`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`name`**        | `string`           |
+# **`provider`**    | `string`           |
+# **`uid`**         | `string`           |
+# **`image`**       | `string`           |
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+#
+
 class User < ActiveRecord::Base
   has_many  :practices 
   validates :name,     presence: true
