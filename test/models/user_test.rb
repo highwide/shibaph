8b91ba84cff03717c69a2a36deab538ce4169442
ta1kt0me <p.wadachi@gmail.com>
2015-06-28 00:11:39 +0900

    Find or create a user from OmniAuth hash

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test 'find existed user' do
+    auth = OmniAuth::AuthHash.new({
+      provider: "twitter",
+      uid:      "123456",
+      info:     {
+        nickname: "johnqpublic",
+        image:    "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png"
+      }
+    })
+    user = User.find_or_create_from_auth_hash(auth)
+    assert(user.id, @user.id)
+    assert_equal(user.uid, auth[:uid])
+  end
+
+  test 'create new user' do
+    auth = OmniAuth::AuthHash.new({
+      provider: "twitter",
+      uid:      "123",
+      info:     {
+        nickname: "johnqpublic",
+        image:    "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png"
+      }
+    })
+    user = User.find_or_create_from_auth_hash(auth)
+    assert_not_equal(user.id, @user.id)
+    assert_equal(user.uid, auth[:uid])
+  end
 end
