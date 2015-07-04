ed5452320a2704a7f6e9bea5d72d5ced63911d0b
ta1kt0me <p.wadachi@gmail.com>
2015-07-04 14:54:25 +0900

    Sign in & Sign up with Twitter auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
