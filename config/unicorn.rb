0ec0b535b12508755991ae59f3a39642017e825a
ta1kt0me <p.wadachi@gmail.com>
2015-07-07 21:36:48 +0900

    Output log to rails log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Rails.logger.info 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -14,7 +14,7 @@ end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    Rails.logger.info 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
