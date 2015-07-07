f0ce169486d99430097ae637b127b91d72930ebc
ta1kt0me <p.wadachi@gmail.com>
2015-07-07 21:39:24 +0900

    Change and to &&

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,7 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
@@ -17,6 +16,5 @@ after_fork do |server, worker|
     Rails.logger.info 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
