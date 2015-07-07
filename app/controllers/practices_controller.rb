dbdc70866713395f7505f0a36345f16c04efac2e
Takahiro Uchiyama <hochweit728@gmail.com>
2015-07-08 00:33:10 +0900

    Create practice#index and practice#show

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,22 +1,29 @@
 class PracticesController < ApplicationController
   def index
+    @user = User.first
+    @practices = @user.practices
   end
 
   def show
+    @practice = Practice.find(params[:id])
+
+    end_date = Time.zone.now
+    start_date = end_date.months_ago(4)
+    @date_per_week = []
+
+    while (start_date <= end_date)
+      if @date_per_week.empty? || @date_per_week.last.last.wday == 6
+        @date_per_week << [start_date]
+      else
+        @date_per_week.last << start_date
+      end
+      start_date += 1.day
+    end
   end
 
   def new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
