2de67258812c2bbb0837ce0dd62d5aa68319f475
ta1kt0me <p.wadachi@gmail.com>
2015-07-13 23:48:49 +0900

    Remove unused case

--- test/controllers/practices_controller_test.rb
+++ test/controllers/practices_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class PracticesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   def setup
     user = User.create(
       provider: 'twitter',
