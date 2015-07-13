562e6ee71eee3631cb63000f7756e12b28c40624
ta1kt0me <p.wadachi@gmail.com>
2015-07-14 00:22:16 +0900

    Format

--- test/controllers/practices_controller_test.rb
+++ test/controllers/practices_controller_test.rb
@@ -4,14 +4,15 @@ class PracticesControllerTest < ActionController::TestCase
   def setup
     user = User.create(
       provider: 'twitter',
-      name: 'John',
-      uid: '1',
-      image: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_2.png'
+      name:     'John',
+      uid:      '1',
+      image:    'https://abs.twimg.com/sticky/default_profile_images/default_profile_2.png'
     )
     session[:user_id] = user.id
   end
+
   test "sohuld post create" do
-    post :create, practice: {title: 'running', unit: 'km'}
+    post :create, practice: { title: 'running', unit: 'km' }
 
     assert_redirected_to root_path
 
