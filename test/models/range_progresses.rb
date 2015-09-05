c00fb7403ac9d3c5df086235b4315eb2ec5b6e10
ta1kt0me <p.wadachi@gmail.com>
2015-09-05 22:18:20 +0900

    Refactor instance method description

--- test/models/range_progresses.rb
+++ test/models/range_progresses.rb
@@ -7,7 +7,7 @@ class RangeProgressesTest < ActiveSupport::TestCase
     @range_progresses = RangeProgresses.new(1, start_date, end_date)
   end
 
-  test 'format_each_week' do
+  test '#format_each_week' do
     result = @range_progresses.format_each_week
     assert { result == [['2015-09-05'.to_date], ['2015-09-06'.to_date, '2015-09-07'.to_date]] }
   end
