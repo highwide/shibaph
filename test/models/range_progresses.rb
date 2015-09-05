ebdc136939174aaf7714c36d22d966d08b7bf3b5
ta1kt0me <p.wadachi@gmail.com>
2015-09-05 22:43:32 +0900

    Add total count test

--- test/models/range_progresses.rb
+++ test/models/range_progresses.rb
@@ -2,13 +2,18 @@ require 'test_helper'
 
 class RangeProgressesTest < ActiveSupport::TestCase
   def setup
-    start_date = '2015-09-05'.to_date # 土曜日
+    start_date = '2015-08-28'.to_date # 土曜日
     end_date = start_date + 2.days
-    @range_progresses = RangeProgresses.new(1, start_date, end_date)
+    progress = daily_progresses :range_progress
+    @range_progresses = RangeProgresses.new(progress.practice.id, start_date, end_date)
   end
 
   test '#format_each_week' do
     result = @range_progresses.format_each_week
-    assert { result == [['2015-09-05'.to_date], ['2015-09-06'.to_date, '2015-09-07'.to_date]] }
+    assert { result == [['2015-08-28'.to_date, '2015-08-29'.to_date], ['2015-08-30'.to_date]] }
+  end
+
+  test '#total_done' do
+    assert_equal 1, @range_progresses.total_done
   end
 end
