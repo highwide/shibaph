07a1c51ff7830a2f8841782c30294a6deed82640
ta1kt0me <p.wadachi@gmail.com>
2015-09-05 13:50:42 +0900

    Add feature spec in top

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
+require 'minitest/rails/capybara'
 
 Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::SpecReporter.new]
 
