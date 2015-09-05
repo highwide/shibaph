da40e91a7133c849f4bc434b4690c0d4585b49de
Takahiro Uchiyama <hochweit728@gmail.com>
2015-09-05 18:12:51 +0900

    リファクタリング開始。Controllerを薄くする

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -32,24 +32,4 @@ class PracticesController < ApplicationController
 
   def update
   end
-
-  private
-
-  #
-  # Viewで表示するgraphのためにデータ構造を整形するメソッド
-  # 日付をkey, DailyProgressモデルをvalueとするHashを作り、
-  # 1週間ごと(土曜日(wdayが6)で切り替え)に別HashとしてArrayに詰め込んでいる
-  #
-  def format_for_graph_of_a_year(progresses)
-    end_date = Time.zone.now.to_date
-    start_date = end_date.years_ago(1).to_date
-    (start_date..end_date).each_with_object([]) do |date, formated_dates|
-      progress = progresses.find{|p| p.done_at == start_date}
-      if formated_dates.empty? || formated_dates.last.any? {|k, _| k.wday == 6}
-        formated_dates << {date => progress}
-      else
-        formated_dates.last[date] = progress
-      end
-    end
-  end
 end
